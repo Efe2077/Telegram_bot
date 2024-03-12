92c998b9da6e8db32edb74efa1c750a23fd76870
Efe <efekochak2017@gmail.com>
2024-03-12 10:28:31 +0300

    Greetings & Goodbyes

--- main.py
+++ main.py
@@ -4,15 +4,25 @@ from random import choice
 
 bot = telebot.TeleBot('6996070096:AAHKAAZEvorjnwrd7Fec9kbYzRSt7qTXV7k')
 
-GREETINGS = ['Привет!', 'Приветствую вас!',
-             'Здравствуйте!', 'Добрый день!',
-             'Салют!', 'Хай!', 'Здорово!',
+GREETINGS = ['Привет', 'Приветствую вас',
+             'Здравствуйте', 'Добрый день',
+             'Салют', 'Хай', 'Здарово',
+             'Прив', 'Дарова',
+             'Здаров', 'Здравия желаю'
              ]
+
+GOODBYES = ['До свидания', 'Всего хорошего',
+             'Всего доброго', 'До встречи',
+             'Прощайте', 'Бывай', 'Пока',
+            ]
 
 
 @bot.message_handler()
 def mees(message):
-    bot.send_message(message.chat.id, choice(GREETINGS))
+    if message.text.capitalize() in GREETINGS:
+        bot.send_message(message.chat.id, choice(GREETINGS))
+    else:
+        bot.send_message(message.chat.id, choice(GOODBYES))
 
 
 @bot.message_handler(commands=['start', 'hello', 'привет'])
